3c2dee7fc4306faa42f7c9ea2209691191527947
FrancoLuisCantero <196580000+FrancoLuisCantero@users.noreply.github.com>
2025-09-10 22:03:35 -0300

    modificar estilos del navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,77 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
-  
+  const icons = {
+    home: {
+      default: "src/assets/icons/home-1.svg",
+      active: "src/assets/icons/home-2.svg"
+    },
+    turnos: {
+      default: "src/assets/icons/turnos-1.svg",
+      active: "src/assets/icons/turnos-2.svg"
+    },
+    reintegros: {
+      default: "src/assets/icons/reintegros-1.svg",
+      active: "src/assets/icons/reintegros-2.svg"
+    },
+    autorizaciones: {
+      default: "src/assets/icons/autorizaciones-1.svg",
+      active: "src/assets/icons/autorizaciones-2.svg"
+    },
+    recetas: {
+      default: "src/assets/icons/recetas-1.svg",
+      active: "src/assets/icons/recetas-2.svg"
+    }
+  };
+
   return (
     <nav>
-      <Link to="/Home">
-        <img src="src/assets/icons/home-1.svg" alt="Ir al inicio" />
-        <p>Home</p>
-      </Link>
-      
-      <Link to="/turnos">
-        <img src="src/assets/icons/turnos-1.svg" alt="Ir a la sección de turnos" />
-        <p>Turnos</p>
-      </Link>
+      <NavLink to="/home">
+        {({ isActive }) => (
+          <>
+            <img src={isActive ? icons.home.active : icons.home.default} alt="Ir al inicio" />
+            <p>Home</p>
+          </>
+        )}
+      </NavLink>
 
-      <Link to="/reintegros">
-        <img src="src/assets/icons/reintegros-1.svg" alt="Ir a la sección de reintegros" />
-        <p>Reintegros</p>
-      </Link>
+      <NavLink to="/turnos">
+        {({ isActive }) => (
+          <>
+            <img src={isActive ? icons.turnos.active : icons.turnos.default} alt="Ir a la sección de turnos" />
+            <p>Turnos</p>
+          </>
+        )}
+      </NavLink>
 
-      <Link to="/autorizaciones">
-        <img src="src/assets/icons/autorizaciones-1.svg" alt="Ir a la sección de autorizaciones" />
-        <p>Autorizaciones</p>
-      </Link>
+      <NavLink to="/reintegros">
+        {({ isActive }) => (
+          <>
+            <img src={isActive ? icons.reintegros.active : icons.reintegros.default} alt="Ir a la sección de reintegros" />
+            <p>Reintegros</p>
+          </>
+        )}
+      </NavLink>
 
-      <Link to="/recetas">
-        <img src="src/assets/icons/recetas-1.svg" alt="Ir a la sección de recetas" />
-        <p>Recetas</p>
-      </Link>
-      
-      {/**
-        <Link to="/prestadores">
-          <img src="src/assets/icons/prestadores-1.svg" alt="Ir a la sección de prestadores" />
-        </Link>
-      */}
+      <NavLink to="/autorizaciones">
+        {({ isActive }) => (
+          <>
+            <img src={isActive ? icons.autorizaciones.active : icons.autorizaciones.default} alt="Ir a la sección de autorizaciones" />
+            <p>Autorizaciones</p>
+          </>
+        )}
+      </NavLink>
+
+      <NavLink to="/recetas">
+        {({ isActive }) => (
+          <>
+            <img src={isActive ? icons.recetas.active : icons.recetas.default} alt="Ir a la sección de recetas" />
+            <p>Recetas</p>
+          </>
+        )}
+      </NavLink>
     </nav>
-  )
+  );
 }
-
