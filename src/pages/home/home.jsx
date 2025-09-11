c8a31ec32f8e32b00ef34142e26e13cca3086904
GabrielFacundoGutierrez <gabrielfacundo.gutierrez@estudiantes.unahur.edu.ar>
2025-09-10 22:13:20 -0300

    cambios minimos en el home

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
                 <strong>Fecha:</strong> Lunes 21 de Septiembre.<br />
                 <strong>Hora:</strong> 10:00 AM
               </p>
-              <button className="btn btn-primary">Ver detalles</button>
+              <button className="btn btn-primary">Ir a Turnos</button>
             </div>
           </div>
         </div>
@@ -36,7 +36,35 @@ export default function Home() {
                 <strong>Medicamento:</strong> Ibuprofeno 600mg.<br />
                 <strong>Dosis:</strong> Cada 8 horas.
               </p>
-              <button className="btn btn-success">Ver receta</button>
+              <button className="btn btn-primary">Ir a Recetas</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="card shadow-lg h-100">
+            <div className="card-body">
+              <h5 className="card-title">Último Reintegro</h5>
+              <p className="card-text">
+                <strong>Aprobada:</strong> Jueves 11 de septiembre.<br />
+                <strong>Especialidad:</strong> Neurocirugia<br />
+                <strong>Reintegro:</strong> $160.000.
+              </p>
+              <button className="btn btn-primary">Ir a Reintegros</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-6">
+          <div className="card shadow-lg h-100">
+            <div className="card-body">
+              <h5 className="card-title">Última Autorizacion</h5>
+              <p className="card-text">
+                <strong>Fecha:</strong> Lunes 21 de septiembre.<br />
+                <strong>paciente:</strong> Franco Cantero<br />
+                <strong>Lugar de Prestacion:</strong> Hospital municipal de Hurlingham
+              </p>
+              <button className="btn btn-primary">Ir a Reintegros</button>
             </div>
           </div>
         </div>
